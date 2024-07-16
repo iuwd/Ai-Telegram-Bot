ab04d77b697ad0f8a433d375d014b993b7acaf84
G0ik <81510568+G0ik@users.noreply.github.com>
2024-07-16 11:41:56 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+from telegram import *
+from telegram.ext import *
+import json
+import google.generativeai as genai
+from typing import Final
+from prompt import *
+
+TOKEN: Final = '7398896187:AAGlB9VhGPg-SbbLI1-okzC3UKFXYpElxec'
+BOT_USERNAME: Final = 'D7oomAi_Bot'
+
+# Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('This is an AI bot, ask anything')
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('This is an AI bot, ask anything')
+
+
+# Responses
+def handle_response(text: str) -> str:
+    pr: str = text.lower()
+    return(ai(pr))
+    
+# Messgae Handling
+async def handle_message(update: Update, Context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_response(new_text)
+
+        else: 
+            return
+            
+    else:
+        response: str = handle_response(text)
+
+    print('Bot:', response)
+    await update.message.reply_text(response)
+
+async def error(update: Update, Context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused the following error {Context.error}')
+
+
+if __name__ == '__main__':
+    print('Starting Bot...')
+    app = Application.builder().token(TOKEN).build()
+
+    #commands
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+
+    #messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    #errors
+    app.add_error_handler(error)
+
+    print('Polling...')
+    app.run_polling(poll_interval=3)
---
+++ prompt.py
@@ -0,0 +1,31 @@
+import json
+import google.generativeai as genai
+
+
+# --- Configuration ---
+genai.configure(api_key='AIzaSyDZUWGSkNnD1HtSrH2z-5JwxtVR1Ha-FsE')
+
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-pro",
+  generation_config=generation_config,
+  system_instruction=[
+  "You are a bot that helps people with educational problems.", 
+  "Don't use rude words.",
+  "GAT is a national test for a highschooler in the Kingdom of Saudi Arabia, It is close to the SAT but has some differences." ],
+)
+
+# --- Functions ---
+def ai(prompt):
+  print(prompt)
+  response = model.generate_content(prompt)
+  while(True):
+    if response._done:
+      return(response.text)
